10b76509a9df819597dc0a9efb0649cb1362d596
Thiago F Macedo <thiago@internetbudi.com.br>
2013-11-22 20:08:18 -0200

    Changes in config design

--- wp-config.php
+++ wp-config.php
@@ -1,64 +1,16 @@
 <?php
 /**
- * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
- * @package WordPress
- */
-
-
-/**
 * Alow local environment configuration
 * 
 * Insert file wp-config-env.php with the above variables and constants
 */
-if ( file_exists( dirname( __FILE__ ) . '/wp-config-env.php' ) )
+if ( file_exists( dirname( __FILE__ ) . '/wp-config-env.php' ) ) {
     include( dirname( __FILE__ ) . '/wp-config-env.php' );
-	// ** MySQL settings - You can get this info from your web host ** //
-	/** The name of the database for WordPress */
-	define('DB_NAME', 'database_name_here');
-
-	/** MySQL database username */
-	define('DB_USER', 'username_here');
-
-	/** MySQL database password */
-	define('DB_PASSWORD', 'password_here');
-
-	/** MySQL hostname */
-	define('DB_HOST', 'localhost');
-
-	/** Database Charset to use in creating database tables. */
-	define('DB_CHARSET', 'utf8');
-
-	/** The Database Collate type. Don't change this if in doubt. */
-	define('DB_COLLATE', '');
+} else {
+	die('You did not configured the system properly. Missing wp-config-env.php')
 }
 
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+include 'wp-config-salt.php'
 
 /**#@-*/
 
@@ -68,7 +20,8 @@ define('NONCE_SALT',       'put your unique phrase here');
  * You can have multiple installations in one database if you give each a unique
  * prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+if (!$table_prefix)
+	$table_prefix  = 'wp_';
 
 /**
  * WordPress Localized Language, defaults to English.
@@ -92,6 +45,25 @@ if ( !defined( 'WP_DEBUG' ) )
 	define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
+
+
+/**
+* Define our default theme
+*/
+define('WP_DEFAULT_THEME', 'wordpress-bootstrap');
+
+/**
+* This lets WordPress know that the core files are in the wordpress directory
+*/
+define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress');
+define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME']);
+
+/**
+* Moving wp-content folder configuration
+*/
+define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content');
+define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
+
 
 
 /** Absolute path to the WordPress directory. */
