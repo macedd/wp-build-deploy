4bd1c987b2e43a605d5d0353cb65ffb087a951fb
Thiago F Macedo <thiago@internetbudi.com.br>
2013-11-22 21:42:44 -0200

    Sample environment config

---
+++ wp-config-env-sample.php
@@ -0,0 +1,18 @@
+<?php
+// Local server settings
+define( 'WP_LOCAL_DEV', true );
+
+// Local Database
+define('DB_NAME', 'wpsite');
+define('DB_USER', 'wpsite');
+define('DB_PASSWORD', 'wpsitep4ss');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+
+// Sets  our local env domains
+define('WP_HOME','http://local.website.com');
+ 
+// Turn on debug for local environment
+define('WP_DEBUG', true);
+
